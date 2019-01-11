0d5602b8904ab066c65654b080ebc63c714c0011
yifangchen <yifangchen@google.com>
2019-01-11 10:47:39 -0500

    address the comments2

--- batch/vep/run_script_with_watchdog.sh
+++ batch/vep/run_script_with_watchdog.sh
@@ -14,51 +14,61 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This script runs another script (first argument) in the background. Every
-# few seconds (second argument), it reads the time in seconds from one watchdog
-# file (third argument) and checks whether the watchdog file is up to date.
-# Once the watchdog file is found to be stale, the background process will be
-# killed.
-
-function get_last_update_time {
-  next_wait_time=0
-  declare -i last_update_time
-  until last_update_time=`gsutil cat ${1}` || [ ${next_wait_time} -eq 4 ]; do
-    sleep $(( next_wait_time++ ))
-  done
-  echo ${last_update_time}
-}
+# This script runs `script_to_run` (first argument) in the background. Every
+# `watchdog_file_update_interval` seconds (second argument), it checks the last
+# update time of `watchdog_file` (third argument). Once the watchdog file is
+# found to be stale, the background process will be killed. The arguments needed
+# for `script_to_run` are passed after the third argument.
 
 set -euo pipefail
 
-script_to_run=${1}
-interval_in_seconds=${2}
-watchdog_file=${3}
+#################################################
+# Returns the generation number of a GCS file. In case of failure, it sleeps and
+# retries for at most 4 times.
+# Arguments:
+#   $1: The GCS file.
+#################################################
+function get_last_update_time {
+  next_wait_time=0
+  until last_update_time_in_microseconds=`gsutil stat ${1} | awk '$1 == "Generation:" {print $2}'` || [ ${next_wait_time} -eq 4 ]; do
+    sleep $(( next_wait_time++ ))
+  done
+  echo ${last_update_time_in_microseconds}
+}
 
+function main {
+  script_to_run=${1}
+  watchdog_file_update_interval=${2}
+  watchdog_file=${3}
+  script_args=${@:4}
+  watchdog_file_stale_time="$((4*${watchdog_file_update_interval}))"
 
-${script_to_run} ${@:4} &
+  ${script_to_run} ${script_args} &
 
-background_pid="$!"
-while ps -p ${background_pid} > /dev/null
-do
-  start=$(get_last_update_time ${watchdog_file})
-  declare -i end
-  end=`date +%s`
-  diff="$((end-start))"
-  echo "The watchdog file is updated ${diff} seconds ago."
-  if (($diff>4*${interval_in_seconds})); then
-    echo "ERROR: The watchdog file is stale, and running of ${script_to_run} has been killed."
-    kill ${background_pid}
+  background_pid="$!"
+  while ps -p ${background_pid} > /dev/null
+  do
+    start="$(($(get_last_update_time ${watchdog_file})/1000000))"
+    declare -i end
+    end=`date +%s`
+    diff="$((end-start))"
+    echo "The watchdog file is updated ${diff} seconds ago."
+    if ((${diff}>${watchdog_file_stale_time})); then
+      echo "ERROR: The watchdog file is stale, and running of ${script_to_run} has been killed."
+      kill "${background_pid}"
+      exit 1
+    else
+      sleep "${watchdog_file_update_interval}"
+    fi
+  done
+  wait "${background_pid}"
+  if [[ $? -ne 0 ]]; then
+    echo "Running of ${script_to_run} failed."
     exit 1
   else
-    echo "Waiting"
-    sleep ${interval_in_seconds}
+    echo "Running of ${script_to_run} succeed."
+    exit 0
   fi
-done
-wait ${background_pid}
-if [[ $? -ne 0 ]]; then
-  echo "Running of ${script_to_run} failed."
-  exit 1
-else
-  echo "Running of ${script_to_run} succeed."
-fi
+}
+
+main "$@"
